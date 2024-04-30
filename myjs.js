eb5301760c2d5708bf985bd02beaf3c4bd559109
adrian <eduardotrevinop@gmail.com>
2024-04-30 07:14:06 -0600

    first commit

---
+++ myjs.js
@@ -0,0 +1,10 @@
+const fechaActual = new Date();
+const dia = fechaActual.getDate();
+const mes = fechaActual.getMonth() + 1;
+const anio = fechaActual.getFullYear();
+
+const fechaFormateada = `${dia}/${mes}/${anio}`;
+document.write(`<p>Fecha actual: ${fechaFormateada}</p>`);
+
+// Tercera parte del mensaje: -S3gur4-
+// Como le podre hacer para que google no indexe la pagina?
