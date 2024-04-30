8c28b44cf333b805926609980a54a0eb95aaf6ca
adrian <eduardotrevinop@gmail.com>
2024-04-30 07:15:14 -0600

    removed dashes

--- myjs.js
+++ myjs.js
@@ -6,5 +6,5 @@ const anio = fechaActual.getFullYear();
 const fechaFormateada = `${dia}/${mes}/${anio}`;
 document.write(`<p>Fecha actual: ${fechaFormateada}</p>`);
 
-// Tercera parte del mensaje: -S3gur4-
+// Tercera parte del mensaje: S3gur4
 // Como le podre hacer para que google no indexe la pagina?
